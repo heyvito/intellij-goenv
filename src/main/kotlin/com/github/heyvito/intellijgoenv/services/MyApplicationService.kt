8b7a4dac93f9db92414bea8d7b368455055bc2c1
GitHub Action <action@github.com>
2022-02-22 13:37:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/heyvito/intellijgoenv/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/heyvito/intellijgoenv/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.heyvito.intellijgoenv.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.heyvito.intellijgoenv.MyBundle
 
 class MyApplicationService {
 
